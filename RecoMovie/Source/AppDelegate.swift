3824721e278bdc253d985fea53102b95c883cb5c
Sune Riedel <sri@planday.com>
2020-09-15 15:29:35 +0200

    Xcode project skeleton added.

---
+++ RecoMovie/Source/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  RecoMovie
+//
+//  Created by Sune Riedel on 15/09/2020.
+//  Copyright © 2020 Sune Riedel. All rights reserved.
+//
+
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController()
+        self.window = window
+        return true
+    }
+}
---
+++ RecoMovie/Source/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RecoMovie
+//
+//  Created by Sune Riedel on 15/09/2020.
+//  Copyright © 2020 Sune Riedel. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
